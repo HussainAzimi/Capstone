7fd25d0ffd929da43ae2d1b82b1d987f06801e38
HussainAzimi <hussainazimi.caree@gmail.com>
2024-11-26 16:06:20 -0600

    working with matt

---
+++ server/routers/userPosts.js
@@ -0,0 +1,44 @@
+
+import { Router } from  'express';
+import userPost from '../models/UserPost.js';
+
+const router = Router();
+
+// Create userPost route
+router.post("/", async (request, response) => {
+  console.log("request", request)
+    try {
+      const newPost = new userPost(request.body);
+
+      const data = await newPost.save();
+
+      response.json(data);
+    } catch(error) {
+      // Output error to the console incase it fails to send in response
+      console.log(error);
+
+      if ('name' in error && error.name === 'ValidationError') return response.status(400).json(error.errors);
+
+      return response.status(500).json(error.errors);
+    }
+  });
+
+
+  // Get all userPost route (Read userPost)
+router.get("/", async (request, response) => {
+    try {
+      // Store the query params into a JavaScript Object
+      const query = request.query; // Defaults to an empty object {}
+
+      const data = await userPost.find(query);
+
+      response.json(data);
+    } catch(error) {
+      // Output error to the console incase it fails to send in response
+      console.log(error);
+
+      return response.status(500).json(error.errors);
+    }
+  });
+
+export default router;
--- store/location.js
+++ store/location.js
@@ -2,7 +2,7 @@ import metroBusStops from './data/Metro_St._Louis_MetroBus_Stops_By_Line';
 export default {
   header: "location page",
   view: "location",
-  category:"education",
+  category:"",
   places: [],
   stops: metroBusStops || []
 };
